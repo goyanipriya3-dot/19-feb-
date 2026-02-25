68433f8d868795201bbbc90d2f078ecb7d458d6d
goyanipriya3-dot <goyanipriya3@gmail.com>
2026-02-25 04:48:14 +0000

    Table

---
+++ Table.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    printf("\n 2x1 = %d", 2*1);
+    printf("\n 2x2 = %d", 2*2);
+    printf("\n 2x3 = %d", 2*3);
+    printf("\n 2x4 = %d", 2*4);
+    printf("\n 2x5 = %d", 2*5);
+    printf("\n 2x6 = %d", 2*6);
+    printf("\n 2x7 = %d", 2*7);
+    printf("\n 2x8 = %d", 2*8);
+    return: 0;
+}
